b307d4c4953efaac896ba3cb4775ae86d1cf454b
Dimosshka25 <ermolaevdima79@gmail.com>
2021-11-25 21:39:43 +0500

    update info about students

--- src/Laba2/info_button.java
+++ src/Laba2/info_button.java
@@ -17,26 +17,25 @@ public class info_button implements ActionListener {
 			
 		JFrame info_GUI = new JFrame("info_button");	// создание графического окна
 		info_GUI.setTitle ("Информация о команде");
-		info_GUI.setBounds(500,400,400,300);
+		info_GUI.setBounds(500,400,400,250);
 		info_GUI.setResizable(false); // фиксированный размер окна
 		
 		JPanel info_panel = new JPanel(); // Панель - используется для организации компонентов в окне
 		info_panel.setLayout(null);
 		info_GUI.add(info_panel);
 		
-		JLabel info1 = new JLabel("ФИО - номер зачетной книжки");
-		info1.setBounds(50,50,200,30);
-		JLabel info2 = new JLabel("ФИО - номер зачетной книжки");
-		info2.setBounds(50,100,200,30);
-		JLabel info3 = new JLabel("ФИО - номер зачетной книжки");
-		info3.setBounds(50,150,200,30);
+		JLabel info1 = new JLabel("Ермолаев Дмитрий Андреевич - 19130110");
+		info1.setBounds(50,50,350,30);
+		JLabel info2 = new JLabel("Судаков Евгений Станиславович - 19130096");
+		info2.setBounds(50,100,350,30);
+		
 		
 		info_panel.add(info1);
 		info_panel.add(info2);
-		info_panel.add(info3);
+		
 		
 		JButton button_exit = new JButton("Назад"); // добавляем кнопку
-		button_exit.setBounds(270,200,100,40);
+		button_exit.setBounds(270,150,100,40);
 		button_exit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 info_GUI.dispose();
