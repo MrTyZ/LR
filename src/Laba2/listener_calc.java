34a9f9e1d81f67499c19bc5b4b9696386828b66b
Dimosshka25 <ermolaevdima79@gmail.com>
2021-12-01 01:15:51 +0500

    Update listener_calc calss

--- src/Laba2/listener_calc.java
+++ src/Laba2/listener_calc.java
@@ -3,17 +3,15 @@ package Laba2;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class listener_calc extends calculation implements ActionListener  {
+public class listener_calc implements ActionListener  {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		text1= subversion.input_text.getText();
-		if (tar2 != 0) {text2= subversion.input_text.getText();}
-		calculate(text1,text2,s1,s2,tar1,tar2,result);
-		
+		// TODO Auto-generated method stub
+		calc Calc = new calc();
 		
 	}
+
 	
 
 }
