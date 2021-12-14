a6bc382fac2c2d4f45c96fd739868d3385f60ebd
MrTyZ <ses0312@yandex.ru>
2021-12-14 20:34:39 +0500

    Update calc class

--- src/Laba2/calc.java
+++ src/Laba2/calc.java
@@ -2,12 +2,10 @@ package Laba2;
 
 public class calc extends calculation{
 	static float tar1=0,tar2=0;
-	private String text11=null,text22=null;
 	public  calc(String text1, float tar1) {
-	this.text11=text1;
 		try {
 			
-			calculate(text11,null,tar1,0);
+			calculate(text1,null,tar1,0);
 		} catch (Exception e) {
 			
 			e.printStackTrace();
@@ -15,10 +13,8 @@ public class calc extends calculation{
 		}
 	}
 	public  calc(String text1, String text2, float tar1, float tar2) {
-		this.text11=text1;
-		this.text22=text2;
 		try {
-			calculate(text11,text22,tar1,tar2);
+			calculate(text1,text2,tar1,tar2);
 		} catch (Exception e) {
 			
 			e.printStackTrace();
