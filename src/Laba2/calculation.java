b38b79b16c535fccbaf0a22a722dfb7bac4ffb81
Dimosshka25 <ermolaevdima79@gmail.com>
2021-12-14 20:06:25 +0500

    Update calculation class

--- src/Laba2/calculation.java
+++ src/Laba2/calculation.java
@@ -1,20 +1,31 @@
 package Laba2;
 
 
+
 public abstract class calculation  {
 	
-	
-	public void calculate(String text1,String text2,float tar1,float tar2) 
+	float s1=0,s2=0,result;
+	public void calculate(String text1,String text2,float tar1,float tar2)
 	{
-		 	float s1=0,s2=0,result=0;
-			s1 = Float.parseFloat(text1);
-			if (tar2 != 0) {s2 = Float.parseFloat(text2);}			
-			result = s1*tar1 + s2*tar2;				
-			subversion.label_result.setText("Результат: " + Float.toString(result));
+		
+		s1 = Float.parseFloat(text1);
+		if (tar2 != 0) {s2 = Float.parseFloat(text2);}
+		ICalc lambda =() -> s1*tar1 + s2*tar2;
+		result = lambda.getResult();
+		subversion.label_result.setText("Результат: " + Float.toString(result));
+		
 			
 	}
+	
+	public interface ICalc {
+		float getResult();
+	}
+	
+	
+	
 }	
 	
 
 
 
+ 
