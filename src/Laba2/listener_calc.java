7feb190d3ea8bf492bc7436530ae4c6a92fea41d
MrTyZ <ses0312@yandex.ru>
2021-12-14 20:46:09 +0500

    Update listener_calc class

--- src/Laba2/listener_calc.java
+++ src/Laba2/listener_calc.java
@@ -4,14 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class listener_calc implements ActionListener  {
-	
+	Tarif tarif = new Tarif();
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (calc.tar2 == 0) {
-			new calc(subversion.input_text1.getText(),calc.tar1);}
+		if (tarif.getTar2() == 0) {
+			new calc(subversion.input_text1.getText(),tarif.getTar1());}
 		else {
-			new calc(subversion.input_text1.getText(),subversion.input_text2.getText(),calc.tar1,calc.tar2);
+			new calc(subversion.input_text1.getText(),subversion.input_text2.getText(),tarif.getTar1(),tarif.getTar2());
 		}
 
 	
