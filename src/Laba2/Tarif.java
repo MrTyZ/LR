9ceb4b67fff89746ffdf2835e19e3e5bb705c5c5
Dimosshka25 <ermolaevdima79@gmail.com>
2021-12-14 20:08:24 +0500

    Add Tarif class

---
+++ src/Laba2/Tarif.java
@@ -0,0 +1,35 @@
+package Laba2;
+
+
+
+public class Tarif {
+
+	private static float tar1=0,tar2=0;
+	
+	
+	
+	public void setTar1(float tar1)
+	{
+		this.tar1=tar1;
+	}
+	
+	public void setTar2(float tar2)
+	{
+		this.tar2=tar2;
+	}
+	
+	public float getTar1()
+	{
+		return tar1;
+	}
+	public float getTar2()
+	{
+		return tar2;
+	}
+
+
+	
+
+	
+	
+}
